eeb680ebdbd3ad27a0038036b812f52b5817967b
Gwladyslsn <gwladyslaisne@outlook.fr>
2026-05-19 15:18:58 +0200

    add form / controller Book

--- app/src/Entity/Book.php
+++ app/src/Entity/Book.php
@@ -41,7 +41,7 @@ class Book
     private ?Editor $editor = null;
 
     #[ORM\ManyToOne(inversedBy: 'books')]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\JoinColumn(nullable: true)] //Remettre false
     private ?Author $author = null;
 
     #[ORM\OneToOne(mappedBy: 'book', cascade: ['persist', 'remove'])]
